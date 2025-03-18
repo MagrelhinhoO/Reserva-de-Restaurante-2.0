77a530112f4a1319905eb02812dd09ebca65bca6
Murilo Eberhardt <murilo.05eberhardt@gmail.com>
2025-03-17 22:13:27 -0300

    Modificação Arquivo Reserva

--- Reserva.java
+++ Reserva.java
@@ -1,17 +1,28 @@
-public class Reserva {
-    private String nome;
-    private String data;
-    private String horario;
-    private int numPessoas;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-    public Reserva(String nome, String data, String horario, int numPessoas) {
-        this.nome = nome;
+public class Reserva implements ICancelavel {
+    protected Cliente cliente;
+    protected LocalDate data;
+    protected LocalTime horario;
+    protected int numPessoas;
+    protected StatusReserva status;
+
+    public Reserva(Cliente cliente, LocalDate data, LocalTime horario, int numPessoas) {
+        this.cliente = cliente;
         this.data = data;
         this.horario = horario;
         this.numPessoas = numPessoas;
+        this.status = StatusReserva.CONFIRMADA;
     }
 
     public void exibirReserva() {
-        System.out.println("Nome: " + nome + ", Data: " + data + ", Horário: " + horario + ", Pessoas: " + numPessoas);
+        System.out.println("Reserva de " + cliente.getNome() + " | Data: " + data + " | Horário: " + horario + " | Status: " + status);
+    }
+
+    @Override
+    public void cancelarReserva() {
+        this.status = StatusReserva.CANCELADA;
+        System.out.println("Reserva cancelada com sucesso.");
     }
 }
